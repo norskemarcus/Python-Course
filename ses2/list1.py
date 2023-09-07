d0b751debdf82bd4d542c1d533c6f100b7c65f9b
Marcus Holje <99866281+norskemarcus@users.noreply.github.com>
2023-09-07 08:31:48 +0200

    last changes

---
+++ ses2/list1.py
@@ -0,0 +1,87 @@
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# To run a .py file from the terminal:
+# Navigate to the directory where your .py script is located
+# python list1.py
+# OBS: just make functions with returns!
+
+
+# A. match_ends
+# Given a list of strings, return the count of the number of
+# strings where the string length is 2 or more and the first
+# and last chars of the string are the same.
+# Note: python does not have a ++ operator, but += works.
+
+
+def match_ends(words):
+    count = sum(1 for word in words if len(word) >= 2 and word[0] == word[-1])
+    return count
+
+
+# B. front_x
+# Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+
+def front_x(words):
+    words_with_x = sorted([word for word in words if word.startswith('x')])
+    words_without_x = sorted(
+        [word for word in words if not word.startswith('x')])
+
+    return words_with_x + words_without_x
+
+
+# C. sort_last
+# Given a list of non-empty tuples, return a list sorted in increasing
+# order by the last element in each tuple.
+# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+# Hint: use a custom key= function to extract the last element form each tuple.
+
+
+def sort_last(tuples):
+    return sorted(tuples, key=lambda x: x[-1])
+
+# In Python, a lambda function is a way to define small, anonymous (unnamed) functions. They are often used when you need a simple function for a short period of time and don't want to define a full function using the def keyword. Lambda functions are often used as arguments for functions that expect a function as input, like the sorted function's key parameter.
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print(f'{prefix} got: {got} expected: {expected}')
+
+
+# Calls the above functions with interesting inputs.
+def main():
+    print('match_ends')
+    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+    test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+    print()
+    print('front_x')
+    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+    print()
+    print('sort_last')
+    test(sort_last([(1, 3), (3, 2), (2, 1)]),
+         [(2, 1), (3, 2), (1, 3)])
+    test(sort_last([(2, 3), (1, 2), (3, 1)]),
+         [(3, 1), (1, 2), (2, 3)])
+    test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+         [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+main()
